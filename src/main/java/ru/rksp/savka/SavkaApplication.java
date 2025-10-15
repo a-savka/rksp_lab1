5b413d4f19de846aeaa8e63a3c3780f449ae2def
Andrey Savka <a_sawka@yahoo.com>
2025-10-15 18:33:27 +0300

    create project

---
+++ src/main/java/ru/rksp/savka/SavkaApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.savka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/savka/SavkaApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.savka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
